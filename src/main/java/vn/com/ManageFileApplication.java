3a1ce5fd264f07d18185b09edd3f729173618476
kieutien <kieunt.2501@gmail.com>
2025-06-28 21:15:21 +0700

    first commit

---
+++ src/main/java/vn/com/ManageFileApplication.java
@@ -0,0 +1,13 @@
+package vn.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageFileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageFileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/com/ManageFileApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
